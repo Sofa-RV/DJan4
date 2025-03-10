963006d8a68bf4b8f2c4f35ca88fad70b4d38e3a
sofa_r.v <sofya.komolykh@bk.ru>
2025-03-11 02:18:25 +0300

    add files

--- website/settings.py
+++ website/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'school',
-    'debug_toolbar',
 
 ]
 
@@ -47,7 +46,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'website.urls'
